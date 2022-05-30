2ef692bd3edbc881b1695f0b4dba4a36788edd1f
EzequielTecsid <ezequiel@tecsid.com>
2022-05-29 23:04:36 -0300

    Mostrar mensajes en la interfa

--- js/app.js
+++ js/app.js
@@ -25,9 +25,62 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
+// Proto para mostrar mensaje de error
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement('DIV');
+
+    if(tipo === 'error'){
+        div.classList.add('error');
+    }else{
+        div.classList.add('correcto');
+    }
+
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    // Insertar en el HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado')); 
+    
+    // Eliminar
+    setTimeout(()=>{
+        div.remove();
+    },3000);
+}
+
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', () => {
     
     ui.llenarOpciones(); // Llena el select con los anios
-})
+});
+
+eventListeners();
+function eventListeners(){
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e){
+    e.preventDefault();
+
+    // Leer la marca seleccionada
+    const marca = document.querySelector('#marca').value;
+    
+    // Leer el anio seleccionado
+    const anio = document.querySelector('#year').value;
+
+    // Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    
+    if(marca === '' || anio === '' || tipo === ''){
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return;
+    }
+
+    ui.mostrarMensaje('Cotizando...', 'exito');
+
+    // Instanciar el seguro
+
+    // Utilizar el prototype que va a cotizar
+}
